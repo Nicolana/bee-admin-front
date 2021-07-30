373e1de96d3545ca7b060ecdcca4f31ee6f4164c
之一 <nicolanazwj@gmail.com>
2021-07-30 09:20:04 +0800

    feat: login 页面更高样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
-import { LoginScreen } from "./screens/login";
+import { LoginScreen } from "./pages/login";
 
 function App() {
   return (
     <div className="App">
         <div className="login-wrap">
-            <div style={{fontSize: "30px", marginBottom: "10px"}}>Login</div>
             <LoginScreen></LoginScreen>
         </div>
     </div>
